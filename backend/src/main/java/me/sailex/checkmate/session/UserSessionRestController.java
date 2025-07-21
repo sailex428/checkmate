7e8e16397135f6dc69fe164549195a7fedc1fcb2
sailex <elias.hahn@mail.de>
2025-07-21 23:20:40 +0200

    finally fixed cors problem

--- backend/src/main/java/me/sailex/checkmate/session/UserSessionRestController.java
+++ backend/src/main/java/me/sailex/checkmate/session/UserSessionRestController.java
@@ -4,13 +4,11 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import me.sailex.checkmate.config.APIPaths;
 import me.sailex.checkmate.config.CheckmateProperties;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000/")
 public class UserSessionRestController {
 
     public static final String TOKEN_NAME = "CHECKMATE-SESSION";
@@ -28,7 +26,6 @@ public class UserSessionRestController {
         if (playerSession == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
         LoginResponse loginResponse = new LoginResponse(playerSession.username());
         return ResponseEntity.ok(loginResponse);
     }
@@ -40,7 +37,6 @@ public class UserSessionRestController {
     ) {
         PlayerSession playerSession = sessionManager.createSession(username);
         response.addCookie(buildCookie(playerSession.token()));
-        response.addHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
 
         LoginResponse loginResponse = new LoginResponse(playerSession.username());
         return ResponseEntity.ok(loginResponse);
