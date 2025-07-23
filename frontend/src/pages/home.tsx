671749b91cf42cc7f3f6f5e70a3fdbeabcc64557
sailex <elias.hahn@mail.de>
2025-07-23 11:38:41 +0200

    adjust gap between header and loginForm

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -2,7 +2,7 @@ import PlayerLoginForm from "../components/home/playerLoginForm.tsx";
 
 const Home = () => {
   return (
-    <div className={"flex flex-col h-3/4 justify-center gap-15"}>
+    <div className={"flex flex-col h-3/4 justify-center gap-12"}>
       <div className={"text-center"}>
         <h1 className={"text-6xl font-bold"}>Checkmate</h1>
         <p className={"text-gray-500 text-xl"}>A simple chess webapp.</p>
