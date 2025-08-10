5c4ff06f18ce6dc2f55246b899e8c3106a4cce8d
sailex <elias.hahn@mail.de>
2025-08-10 15:42:12 +0200

    add github link to navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,12 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChessBishop } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center mx-3 mt-3 px-6 py-4 gap-2 border border-gray-800 rounded-full">
-      <FontAwesomeIcon icon={faChessBishop} className={"text-sky-500"} />
-      <a href={"/"} className={"font-semibold"}>
+    <nav className="flex items-center justify-between m-3 px-6 py-4 border border-gray-800 rounded-full">
+      <a href={"/"} className={"flex items-center gap-2 font-semibold"}>
+        <FontAwesomeIcon icon={faChessBishop} className={"text-sky-500"} />
         Checkmate
+      </a>
+      <a
+        href={"https://github.com/sailex428/checkmate"}
+        className={"font-semibold"}
+        target={"_blank"}
+      >
+        <FontAwesomeIcon icon={faGithub} className={"text-sky-500"} />
       </a>
     </nav>
   );
